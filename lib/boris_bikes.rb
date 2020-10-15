ac6484b393d1e59e89d6139fe555ba23b08cd4e9
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-10-15 14:53:11 +0100

    challenge12

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,18 +1,22 @@
 class DockingStation
-#  attr_reader :available_bikes
+  attr_reader :available_bikes
 
   def initialize
-#    @available_bikes = []
+   @available_bikes
   end
 
   def release_bike
-    return Bike.new
+    fail 'No bikes available' if @available_bikes == nil
+
+    return @available_bikes
   end
 
   def dock_bike(returned_bike)
-#    @available_bikes << returned_bike
+   @available_bikes = returned_bike
   end
+
 end
+
 
 class Bike
   def working?
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -2,21 +2,24 @@ require 'boris_bikes'
 
 RSpec.describe DockingStation do
   it {is_expected.to respond_to(:release_bike)}
+  it {is_expected.to respond_to(:available_bikes)}
+
   it 'does release_bike return an instance of the Bike class' do
+    subject.dock_bike(Bike.new)
     expect(subject.release_bike).to be_an_instance_of(Bike)
   end
   it 'does docking_station dock bike' do
     expect(subject).to respond_to(:dock_bike)
   end
   it 'docking process accepts a Bike as an argument' do
-    subject.dock_bike(Bike.new)
     expect(subject).to respond_to(:dock_bike).with(1).arguments
   end
 
-  it 'accepts returned bike' do
-    expect(subject.available_bikes).not_to eq(nil)
-    #expect(subject.available_bikes).not_to eq([])
-    expect(subject.available_bikes.length).to eq(1)
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 end
 
